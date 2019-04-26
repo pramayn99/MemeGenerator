245e9a386d9a090935ce7ab6d4f6cfddcec0d7cd
Pramay Nikhade <pramayn99@yahoo.com>
2019-04-26 10:57:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import App from "./app"
+import "./styles.css";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

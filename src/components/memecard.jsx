597a4d467491caa21bbb766ca867c7c7e6caefa3
Pramay Nikhade <pramayn99@yahoo.com>
2019-04-26 17:53:20 +0530

    API handled, modal added

--- src/components/memecard.jsx
+++ src/components/memecard.jsx
@@ -1,14 +1,87 @@
-import React from "react";
-import { Card, CardImg, Button, CardTitle, CardText } from "reactstrap";
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  Button,
+  CardTitle,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from "reactstrap";
 
-const MemeCard = props => {
-  console.log(props);
-  return (
-    <Card body className="text-center meme-card">
-      <CardTitle className="card-title">{props.meme.name}</CardTitle>
-      <CardImg bottom width="100%" src={props.meme.url} alt="Card image cap" />
-    </Card>
-  );
-};
+class MemeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Card body className="text-center meme-card" onClick={this.toggle}>
+          <CardTitle className="card-title">{this.props.meme.name}</CardTitle>
+          <CardImg
+            bottom
+            width="100%"
+            src={this.props.meme.url}
+            alt="Card image cap"
+          />
+        </Card>
+        <Modal
+          centered
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader className="text-info" toggle={this.toggle}>
+            Create Meme
+          </ModalHeader>
+          <ModalBody>
+            <img
+              className="modal-image"
+              width="100%"
+              src={this.props.meme.url}
+              alt="Card image cap"
+            />
+            <br />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>Top Text</InputGroupText>
+              </InputGroupAddon>
+              <Input />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>Bottom Text</InputGroupText>
+              </InputGroupAddon>
+              <Input />
+            </InputGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button className="sharp" color="success" onClick={this.handleMeme}>
+              Get Meme
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MemeCard;

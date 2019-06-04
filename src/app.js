8e8e4d243ceeb3863d4e4f3884b451d3c0af4c9e
Pramay Nikhade <pramayn99@yahoo.com>
2019-06-04 17:29:04 +0530

    Bug
    Bug fix

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Button } from "reactstrap";
 import MemeCard from "./components/memecard";
 
-function App()
-
 class App extends Component {
   constructor(props) {
     super(props);
